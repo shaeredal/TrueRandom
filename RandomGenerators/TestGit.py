a67b0f06c3b7b63d73563a4f97a70b788e6860a5
shaeredal <shaeredal@gmail.com>
2015-09-28 18:43:29 +0300

    Test
    test

--- RandomGenerators/TestGit.py
+++ RandomGenerators/TestGit.py
@@ -1,5 +1,12 @@
-__author__ = 'Матвей'
+__author__ = 'Матвей долбаеб'
 print("It's works!!!")
 print("It's works!")
 print("It's works!")
 print("It's works!")
+
+
+def main():
+    return 42
+
+if __name__ = "__main__":
+    main()
