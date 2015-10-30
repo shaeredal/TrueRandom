854402b9193b37a9c1dc331a2becb8a62c198ffe
shaeredal <shaeredal@gmail.com>
2015-10-30 21:10:51 +0300

    upd

--- RandomGenerators/Thread_Random.py
+++ RandomGenerators/Thread_Random.py
@@ -5,7 +5,6 @@ import time
 
 myNum = 0
 
-
 def adder():
     global myNum
     while True:
@@ -15,26 +14,37 @@ def adder():
 
 
 def generate(count):
+    threads = []
     for n in range(8):
-        threading.Thread(target=adder).start()
+        threads.append(threading.Thread(target=adder))
+        threads[n].start()
 
     f = open('D:\\test', 'w')
-
+    myStr = ''
     for n in range(count):
-        time.sleep(0.001)
+        time.sleep(0.0001)
         theNum = myNum % 2
         print(theNum)
-        f.write(str(theNum) + '\n')
+        myStr += (str(theNum) + '\n')
 
+    f.write(myStr)
     f.close()
 
 
 def make_numbers():
     f = open('D:\\test', 'r')
-
+    fu = open('D:\\numberzz', 'w')
+    for i in range(32):
+        newNum = 0
+        for j in range(32):
+            newNum *= 2
+            newNum += int(f.readline())
+        fu.write(str(newNum) + '\n')
 
     f.close()
+    fu.close()
 
 
 if __name__ == '__main__':
     generate(1024)
+    make_numbers()
