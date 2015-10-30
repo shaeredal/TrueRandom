dd5a16f41424f045e923064e4c45e7b0f590c39d
ChasingRainbows <mta05095212@gmail.com>
2015-10-30 17:50:14 +0200

    88
    88

--- RandomGenerators/test.py
+++ RandomGenerators/test.py
@@ -1,1 +1,9 @@
-__author__ = 'Дмитрий'
+import random
+with open('numbers.txt', 'w') as f:
+    f.writelines('{}\n'.format(random.randint(0, 1)) for _ in range(50000))
+
+with open('numbers.txt') as f:
+    s = f.read().replace(' ', '')
+
+with open('numbers.txt', 'w') as h:
+    h.write(s)
