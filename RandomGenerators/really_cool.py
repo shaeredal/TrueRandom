d6e5f0bfcfe82130b7c6dee129bff5ff1091d862
shaeredal <shaeredal@gmail.com>
2015-12-06 18:33:15 +0300

    update

--- RandomGenerators/really_cool.py
+++ RandomGenerators/really_cool.py
@@ -17,10 +17,31 @@ stream = pa.open(format=FORMAT,
                  input=True,
                  frames_per_buffer=INPUT_FRAMES_PER_BLOCK)
 
+
 with open('entropy.txt', 'w') as f:
+    b = bitarray.bitarray()
     for i in range(1000):
         block = stream.read(INPUT_FRAMES_PER_BLOCK)
         shorts = struct.unpack('4h', block)
         for val in shorts:
             print(val)
-            f.write('{0}\n'.format(val & 1))
+            bit = val & 1
+            b.append(bit)
+            print(bit)
+            f.write('{0}\n'.format(bit))
+    print(b)
+    #for i in b:
+    #    print(i)
+    #print(b[0])
+    print(type(b))
+    bias = bitarray.bitarray()
+    bias = [b[i] for i in range( 0 ,len(b),2) if b[i] != b[i+1]]
+    with open('filtered.txt', 'w') as f:
+        for i in bias:
+            if i:
+                f.write('1\n')
+            else:
+                f.write('0\n')
+    print(bias)
+    #print(b.tobytes())
+
